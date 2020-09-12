118a240d1ded883658257ce83e11bdbff5ec003c
Joseph Herrera <tony.herrera90@gmail.com>
2020-09-12 14:25:11 -0500

    addition 3

--- src/com/skilldistillery/jet/AirField.java
+++ src/com/skilldistillery/jet/AirField.java
@@ -2,9 +2,9 @@ package com.skilldistillery.jet;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class AirField {
 
@@ -35,5 +35,52 @@ public class AirField {
 		} catch (Exception e) {
 			System.err.println(e);
 		}
+	}
+
+	public void JetMenu() {
+
+		Scanner sc = new Scanner(System.in);
+//		 switch statement here
+		boolean keepGoing = true;
+		while (keepGoing) {
+			// Enter user options here for next part
+			System.out.println("Enter 1 for the fleet list");
+			System.out.println("Enter 2 to fly all jets.");
+			System.out.println("Enter 3 to view the fastest jet.");
+			System.out.println("Enter 4 to view jet with the longest range.");
+			System.out.println("Enter 4 to view jet with the longest range.");
+			System.out.println("Enter 6 DogFight");
+			System.out.println("Enter 7 to add jet to the fleet.");
+			System.out.println("Enter 8 to remove a jet from fleet.");
+			System.out.println("Enter 9 to quit.");
+			String userInput = sc.next();
+			switch (userInput) {
+
+			case "1":
+
+				System.out.println("Would you like to make another selection?");
+				break;
+
+			case "2":
+				break;
+			case "3":
+				break;
+			case "4":
+				break;
+			case "5":
+				break;
+			case "6":
+				break;
+			case "7":
+				break;
+			case "8":
+				break;
+			case "9":
+				System.out.println("Good-Bye!");
+				keepGoing = false;
+			default:
+			}
+		}
+		sc.close();
 	}
 }
